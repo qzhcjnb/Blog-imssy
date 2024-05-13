5987f00e6693e65fa50ad6e9e6035e44e9739fc3
imsyy <one@imsyy.top>
2024-05-13 17:48:03 +0800

    🌈 style: 优化友链样式

--- .vitepress/theme/assets/rewardData.mjs
+++ .vitepress/theme/assets/rewardData.mjs
@@ -6,6 +6,12 @@ const appreciateData = [
     message: "感谢大佬的分享，我使用了您的开源项目，非常不错，感谢您的分享",
     date: "2024/05/12",
   },
+  {
+    name: "**伟",
+    money: 1.99,
+    message: "支持一下~",
+    date: "2024/04/21",
+  },
 ];
 
 export default appreciateData;
--- .vitepress/theme/views/Thanks.vue
+++ .vitepress/theme/views/Thanks.vue
@@ -110,6 +110,18 @@ import rewardData from "@/assets/rewardData";
           }
         }
       }
+      @media (max-width: 1200px) {
+        grid-template-columns: repeat(4, 1fr);
+      }
+      @media (max-width: 992px) {
+        grid-template-columns: repeat(3, 1fr);
+      }
+      @media (max-width: 768px) {
+        grid-template-columns: repeat(2, 1fr);
+      }
+      @media (max-width: 576px) {
+        grid-template-columns: 1fr;
+      }
     }
   }
 }
